931cb711819a33cc9922b74d339cf2e9281ed74d
Joshua Melvin <joshua.melvin@outlook.com>
2016-04-12 20:01:57 -0500

    added privacy page placeholder

--- server.js
+++ server.js
@@ -2,15 +2,15 @@ var restify = require('restify');
 var builder = require('botbuilder');
 
 // Get secrets from server environment
-var botConnectorOptions = { 
-    appId: process.env.BOTFRAMEWORK_APPID, 
-    appSecret: process.env.BOTFRAMEWORK_APPSECRET 
+var botConnectorOptions = {
+    appId: process.env.BOTFRAMEWORK_APPID,
+    appSecret: process.env.BOTFRAMEWORK_APPSECRET
 };
 
 // Create bot
 var bot = new builder.BotConnectorBot(botConnectorOptions);
 bot.add('/', function (session) {
-    
+
     //respond with user's message
     session.send("You said " + session.message.text);
 });
@@ -23,10 +23,11 @@ server.post('/api/messages', bot.verifyBotFramework(), bot.listen());
 
 // Serve a static web page
 server.get(/.*/, restify.serveStatic({
-	'directory': '.',
-	'default': 'index.html'
+	'directory': __dirname,
+	'default': 'index.html',
+	'privacy': 'privacy.html'
 }));
 
 server.listen(process.env.port || 3978, function () {
-    console.log('%s listening to %s', server.name, server.url); 
+    console.log('%s listening to %s', server.name, server.url);
 });
